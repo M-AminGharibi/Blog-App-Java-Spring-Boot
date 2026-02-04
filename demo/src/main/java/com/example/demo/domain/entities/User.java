2bad7d4369ea5ed392abd7cd592771c387749a4a
Amin Gharibi <amingharibi5@gmail.com>
2026-02-04 21:49:57 +0330

    Setup and Create Entities

--- demo/src/main/java/com/example/demo/domain/entities/User.java
+++ demo/src/main/java/com/example/demo/domain/entities/User.java
@@ -4,12 +4,15 @@ package com.example.demo.domain.entities;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Objects;
 import java.util.UUID;
 
 @Entity
 @Table(name = "users")
-@AllArgsConstructor
 @NoArgsConstructor
+@AllArgsConstructor
 @Getter
 @Setter
 @Builder
@@ -18,4 +21,39 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String password;
+
+    @OneToMany
+    private List<Post> posts;
+
+
+    @Column(nullable = false)
+    private LocalDateTime createAt;
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id) && Objects.equals(email, user.email) && Objects.equals(name, user.name) && Objects.equals(password, user.password) && Objects.equals(createAt, user.createAt);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, email, name, password, createAt);
+    }
+
+
+    @PrePersist
+    protected void onCreate() {
+        this.createAt = LocalDateTime.now();
+    }
+
 }
